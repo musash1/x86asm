9df04ceec13a2f06e62f38104d614a89a85920af
musash1 <luis.gjokaj@gmail.com>
2023-12-15 13:02:55 +0000

    stack

--- hello-world/hello-world.asm
+++ hello-world/hello-world.asm
@@ -1,24 +1,23 @@
 # x86 Assembly Tutorial 001
 
 .global _start
-.intel_syntax
-.section .text
+section .text
 
 _start:
     # write a string to stdout
-    mov %eax, 4
-    mov %ebx, 1 # STDOUT, wherre were writing
-    lea %ecx, [message] 
-    mov %edx, 13
+    mov eax, 4
+    mov ebx, 1 # STDOUT, wherre were writing
+    lea ecx, [message] 
+    mov edx, 13
     int 0x80
 
     #exit syscall
-    mov %eax, 1
-    mov %ebx, 65
+    mov eax, 1
+    mov ebx, 65
     int 0x80
 
 
-.section .data
+section .data
     message:
     .ascii "Hello, World\n"
 
